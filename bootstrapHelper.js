5e7c60028f8a640334dc24562bed70df5769c9f0
ssingh <sandy.boon@gmail.com>
2020-03-03 20:01:57 -0500

    fixing silly errors

--- bootstrapHelper.js
+++ bootstrapHelper.js
@@ -1,23 +1,31 @@
-'use strict';
+('use strict');
 
-import helper from './helper.js';
+const bootStrapHelper = {};
 
-const bootStrapHelper = bootStrapHelper || {};
-
-bootStrapHelper.getBootStrapGridRow = function(id, elementType, additionalClasses) {
-    const bootstrapRow = document.createElement(elementType);
-    id = helper.checkIfValueIsPresent(id) ? id : helper.getNextId();
-    bootstrapRow.setAttribute('id', id);
-    let bootStrapClass = helper.prependStringIfMissing(additionalClasses, 'row');
-    return bootstrapRow;
+bootStrapHelper.getBootStrapGridRow = function(
+  id,
+  elementType,
+  additionalClasses
+) {
+  const bootstrapRow = document.createElement(elementType);
+  id = helper.checkIfValueIsPresent(id) ? id : helper.getNextId();
+  bootstrapRow.setAttribute('id', id);
+  let bootStrapClass = helper.prependStringIfMissing(additionalClasses, 'row');
+  bootstrapRow.setAttribute('class', bootStrapClass);
+  return bootstrapRow;
 };
 
-bootStrapHelper.getootStrapGridColumn = function(id, elementType, additionalClasses) {
-    const bootstrapColumn = document.createElement(elementType);
-    id = helper.checkIfValueIsPresent(id) ? id : helper.getNextId();
-    bootstrapRow.setAttribute('id', id);
-    let bootStrapClass = helper.prependStringIfMissing(additionalClasses, 'col');
-    return bootstrapRow;
+bootStrapHelper.getootStrapGridColumn = function(
+  id,
+  elementType,
+  additionalClasses
+) {
+  const bootstrapColumn = document.createElement(elementType);
+  id = helper.checkIfValueIsPresent(id) ? id : helper.getNextId();
+  bootstrapColumn.setAttribute('id', id);
+  let bootStrapClass = helper.prependStringIfMissing(additionalClasses, 'col');
+  bootstrapColumn.setAttribute('class', bootStrapClass);
+  return bootstrapColumn;
 };
 
-export default bootStrapHelper;
+// export default bootStrapHelper;
--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-'use strict';
-
-import bootStrapHelper from './bootstrapHelper.js';
-
+('use strict');
 //Global initializations
 let welcomeHeading = document.getElementById('pageHeading');
 let timerSpan = document.getElementById('timer');
 let mainContentBlock = document.getElementById('mainContent');
+let mainContainer = document.getElementById('mainContainer');
 
 let initWelcomeBlock = function init() {
-    welcomeHeading.textContent = 'Coding Quiz Challenge';
-    //reset the timer text
-    timerSpan.textContent = 'Time : 0';
-    //add a descriptive paragraph about the quiz, scoring and timer rules.
-    const quizDescription = document.createElement('p');
-    quizDescription.textContent = 'Try to answer the following code related questions with in the time limit. Keep in mind that incorrect answers will penalize your score time by ten seconds!';
-    const quizDescriptionRow = bootStrapHelper.getBootStrapGridRow('quizDescRow', 'div').appendChild(bootStrapHelper.getootStrapGridColumn('quizDescCol', 'div');
-    const quizDescriptionColumn = bootStrapHelper.getootStrapGridColumn('quizDescCol', 'div');
-    mainContentBlock.appendChild(quizDescriptionRow);
+  welcomeHeading.textContent = 'Coding Quiz Challenge';
+  //reset the timer text
+  timerSpan.textContent = 'Time : 0';
+  //add a descriptive paragraph about the quiz, scoring and timer rules.
+  const quizDescription = document.createElement('p');
+  quizDescription.textContent =
+    'Try to answer the following code related questions with in the time limit. Keep in mind that incorrect answers will penalize your score time by ten seconds!';
+  const quizDescriptionRow = bootStrapHelper.getBootStrapGridRow(
+    'quizDescRow',
+    'div'
+  );
+  console.log(quizDescriptionRow);
+  const quizDescCol = bootStrapHelper.getootStrapGridColumn(
+    'quizDescCol',
+    'div'
+  );
+  quizDescCol.appendChild(quizDescription);
+  quizDescriptionRow.appendChild(quizDescCol);
+  console.log(quizDescriptionRow);
+  mainContainer.appendChild(quizDescriptionRow);
 };
 
 initWelcomeBlock();

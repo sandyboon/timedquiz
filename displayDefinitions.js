bdd400d3b2076b82281b9f53b4a6a98fe8a62d39
ssingh <sandy.boon@gmail.com>
2020-03-03 21:35:18 -0500

    refactoring a bit

---
+++ displayDefinitions.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let allTheContent = {
+  quizDescription:
+    'Try to answer the following code related questions with in the time limit. Keep in mind that incorrect answers will penalize your score time by ten seconds!',
+  welcomeHeading: 'Coding Quiz Challenge'
+};
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 ('use strict');
 //Global initializations
-let welcomeHeading = document.getElementById('pageHeading');
-let timerSpan = document.getElementById('timer');
-let mainContentBlock = document.getElementById('mainContent');
-let mainContainer = document.getElementById('mainContainer');
+const welcomeHeading = document.getElementById('contentHeading');
+const timerSpan = document.getElementById('timer');
+const mainContentBlock = document.getElementById('mainContent');
+const mainContainer = document.getElementById('mainContainer');
 
 let initWelcomeBlock = function init() {
   welcomeHeading.textContent = 'Coding Quiz Challenge';
@@ -11,16 +11,15 @@ let initWelcomeBlock = function init() {
   timerSpan.textContent = 'Time : 0';
   //add a descriptive paragraph about the quiz, scoring and timer rules.
   const quizDescription = document.createElement('p');
-  quizDescription.textContent =
-    'Try to answer the following code related questions with in the time limit. Keep in mind that incorrect answers will penalize your score time by ten seconds!';
+  quizDescription.textContent = allTheContent.quizDescription;
   const quizDescriptionRow = bootStrapHelper.getBootStrapGridRow(
     'quizDescRow',
     'div'
   );
-  console.log(quizDescriptionRow);
   const quizDescCol = bootStrapHelper.getootStrapGridColumn(
     'quizDescCol',
-    'div'
+    'div',
+    'col mx-auto'
   );
   quizDescCol.appendChild(quizDescription);
   quizDescriptionRow.appendChild(quizDescCol);
